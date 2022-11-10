4a27c8814712f44f3eacfda8150f31508f64f875
joekroese <joekroese@gmail.com>
2022-11-10 12:06:12 +0000

    add time to saved file name

--- src/routes/SaveData.svelte
+++ src/routes/SaveData.svelte
@@ -7,8 +7,23 @@
     let blob = new Blob([JSON.stringify(output)], {
       type: "application/json",
     });
-    saveAs(blob, "export.json");
+    let d = new Date();
+    let formatted_date =
+      d.getFullYear() +
+      "-" +
+      (d.getMonth() + 1) +
+      "-" +
+      d.getDate() +
+      "_" +
+      d.getHours() +
+      "-" +
+      d.getMinutes() +
+      "-" +
+      d.getSeconds();
+    let extension = ".geojson";
+    let file_name = "data-walk_" + formatted_date + extension;
+    saveAs(blob, file_name);
   }
 </script>
 
-<button on:click={saveJson}>Export GeoJson</button>
+<button on:click={saveJson}>Export Data</button>
